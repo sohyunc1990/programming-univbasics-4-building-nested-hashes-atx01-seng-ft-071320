51d29dff568dcf1277beaa735ceddfdada974944
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 20:54:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   # complexity.
   
   monopoly = {
-    :railroads => {
+    :railroads = {
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25, 
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
         :three_pieces_owned => 100, 
         :four_pieces_owned =>200
       },
-      :names => {
+      :names = {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
