007724e3cacfab5930819bb634a8579ea941f2ec
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 20:42:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,9 @@ def monopoly_with_second_tier
   # complexity.
   monopoly = {
     :railroads => {
-      :pieces => {"4"}
-      :rent_in_dollars => {}
+      :pieces => {"4"},
+      :rent_in_dollars => {},
+      :names => {}
       
     }
   }
