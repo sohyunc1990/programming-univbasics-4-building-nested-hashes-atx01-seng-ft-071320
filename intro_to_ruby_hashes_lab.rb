ae2f3ee78ed80e66a151a14bc0f5f63478caa35e
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 20:58:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
-        :shortline_railraod => {}
+        :shortline_railroad => {}
        }
     }
   }
